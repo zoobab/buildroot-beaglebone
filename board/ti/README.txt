16d58e299c778ed506418145671bb3d0b55af79b
Frank Hunleth <fhunleth@troodon-software.com>
2011-12-11 15:25:58 -0500

    Initial commit of beaglebone patches for building u-boot and the kernel

---
+++ board/ti/README.txt
@@ -0,0 +1,3 @@
+The beaglebone directory contains configurations and patches that are sourced by buildroot. 
+The other directories contain patches or other files that were copied verbatem from Angstrom. The logic is to keep the Angstrom directories
+similar to their originals so that it is easier to pull in updates.
---
+++ board/ti/beaglebone/u-boot/create_softlinks.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# See meta-ti/recipes-bsp/u-boot/u-boot_2011.10rc.bb for patch list
+# used in Angstrom.
+ln -s ../../u-boot/2011.09git/0001-am335x_evm-only-do-in-kernel-dhcp-when-using-NFS-use.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0001-am335x_evm-only-do-in-kernel-dhcp-when-using-NFS-use.patch
+ln -s ../../u-boot/2011.09git/0002-am335x_evm-boot-kernel-from-boot-in-ext2-3-filesyste.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0002-am335x_evm-boot-kernel-from-boot-in-ext2-3-filesyste.patch
+ln -s ../../u-boot/2011.09git/0003-am335x_evm-set-bootdelay-to-1.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0003-am335x_evm-set-bootdelay-to-1.patch
+ln -s ../../u-boot/2011.09git/0004-am335x-evm-make-MMC-rootfs-RO-on-boot-so-fsck-works.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0004-am335x-evm-make-MMC-rootfs-RO-on-boot-so-fsck-works.patch
+ln -s ../../u-boot/2011.09git/0005-am335x-Change-mmc_load_uimage-to-load-at-a-fixed-add.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0005-am335x-Change-mmc_load_uimage-to-load-at-a-fixed-add.patch
+ln -s ../../u-boot/2011.09git/0006-am335x-evm-Fix-bone-pmic-shut-down-over-USB-power.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0006-am335x-evm-Fix-bone-pmic-shut-down-over-USB-power.patch
+ln -s ../../u-boot/2011.09git/0007-am335x_evm-switch-to-ext4.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0007-am335x_evm-switch-to-ext4.patch
+ln -s ../../u-boot/2011.09git/0008-HACK-am335x-evm-turn-d-cache-on-globally-turn-it-off.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0008-HACK-am335x-evm-turn-d-cache-on-globally-turn-it-off.patch
+ln -s ../../u-boot/2011.09git/0009-am335x-evm-enable-i2c2-pinmux-for-beaglebone.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0009-am335x-evm-enable-i2c2-pinmux-for-beaglebone.patch
