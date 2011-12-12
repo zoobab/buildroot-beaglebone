349bc1e18a75bbc61be3d0d7a7a70761839170c5
Frank Hunleth <fhunleth@troodon-software.com>
2011-12-12 00:09:15 -0500

    Switch to use toolchain from TI SDK and minor updates to get kernel to load

--- board/ti/beaglebone/u-boot/create_softlinks.sh
+++ board/ti/beaglebone/u-boot/create_softlinks.sh
@@ -8,6 +8,6 @@ ln -s ../../u-boot/2011.09git/0003-am335x_evm-set-bootdelay-to-1.patch uboot-f63
 ln -s ../../u-boot/2011.09git/0004-am335x-evm-make-MMC-rootfs-RO-on-boot-so-fsck-works.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0004-am335x-evm-make-MMC-rootfs-RO-on-boot-so-fsck-works.patch
 ln -s ../../u-boot/2011.09git/0005-am335x-Change-mmc_load_uimage-to-load-at-a-fixed-add.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0005-am335x-Change-mmc_load_uimage-to-load-at-a-fixed-add.patch
 ln -s ../../u-boot/2011.09git/0006-am335x-evm-Fix-bone-pmic-shut-down-over-USB-power.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0006-am335x-evm-Fix-bone-pmic-shut-down-over-USB-power.patch
-ln -s ../../u-boot/2011.09git/0007-am335x_evm-switch-to-ext4.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0007-am335x_evm-switch-to-ext4.patch
+###ln -s ../../u-boot/2011.09git/0007-am335x_evm-switch-to-ext4.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0007-am335x_evm-switch-to-ext4.patch
 ln -s ../../u-boot/2011.09git/0008-HACK-am335x-evm-turn-d-cache-on-globally-turn-it-off.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0008-HACK-am335x-evm-turn-d-cache-on-globally-turn-it-off.patch
 ln -s ../../u-boot/2011.09git/0009-am335x-evm-enable-i2c2-pinmux-for-beaglebone.patch uboot-f63b270e47f62f4d1a05b2001357e215966c6f5a-0009-am335x-evm-enable-i2c2-pinmux-for-beaglebone.patch
